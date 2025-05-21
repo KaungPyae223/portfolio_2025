19adbe42c3847e45119ddc43020a4be3e9ae5ff6
Kaung223 <kaungpyaeaung8123@gmail.com>
2025-05-21 10:53:30 +0630

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import NavBar from "@/features/public/nav/NavBar";
 import "./globals.css";
+import Script from "next/script";
+import Analytics from "./analytics";
 
 export const metadata = {
   title: "Kaung Pyae Aung",
@@ -13,8 +15,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XX8Y6TYW6D`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XX8Y6TYW6D', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body className="transition-colors duration-300 bg-white text-black dark:bg-gray-900 dark:text-white">
         <header>
+          <Analytics />
           <NavBar />
         </header>
         <main>{children}</main>
