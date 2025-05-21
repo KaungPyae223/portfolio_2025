45d0fe8fca812c3e82259892c6f0e108ff9b3729
Kaung223 <kaungpyaeaung8123@gmail.com>
2025-05-21 11:00:41 +0630

    fix: fix on the deploy 12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,24 @@
-import NavBar from "@/features/public/nav/NavBar";
-import "./globals.css";
-import Script from "next/script";
-import Analytics from "./analytics";
+"use client";
 
-export const metadata = {
-  title: "Kaung Pyae Aung",
-  description: "Kaung Pyae Aung's portfolio website",
-};
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        {/* Google Analytics Script */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-XX8Y6TYW6D`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XX8Y6TYW6D', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-      </head>
-      <body className="transition-colors duration-300 bg-white text-black dark:bg-gray-900 dark:text-white">
-        <header>
-          <Analytics />
-          <NavBar />
-        </header>
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export default function Analytics() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-XX8Y6TYW6D", {
+        page_path: pathname,
+      });
+    }
+  }, [pathname]);
+
+  return null;
 }
